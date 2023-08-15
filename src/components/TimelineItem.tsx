3eecb1f36b8259fddaf1e643593d0bc2d0579609
mikaelpac <mikaelpac@gmail.com>
2023-08-16 00:31:54 +0300

    font change

--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -24,8 +24,8 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
   return (
     <div className={` rounded-lg relative md:flex md:items-center mb-1 ${flexDirection} md:h-96`}>
       <div className={`md:w-1/2 mx-4 md:items-center content-between pt-3 gap-8 md:px-5`}>
-        <h2 className={`text-xl md:text-3xl text-yellow-500 dark:text-yellow-400 font-bold`}>{duration}</h2>
-        <h3 className="text-xl md:text-3xl text-black dark:text-white  mb-2">{title}</h3>
+        <h2 className={`text-2xl md:text-3xl text-yellow-500 dark:text-yellow-400 font-bold`}>{duration}</h2>
+        <h3 className="text-xl md:text-3xl font-bold md:font-normal text-black dark:text-white  mb-2">{title}</h3>
         <p className="text-lg text-black dark:text-slate-300 mb-4">{description}</p>
       </div>
       <div className={`md:w-1/2 md:max-h-80 flex md:px-5 px-4 justify-start ${reverse ? "" : ""} ${isMobileAppImg ? "max-h-80" : ""}`}>
@@ -38,7 +38,7 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
           className="rounded-md py-2 object-contain transition ease-in-out md:hover:scale-103 transform "
         />
       </div>
-      {title !== "AO Solutions Oy" && <hr className="h-px border-0 my-10 mx-2 bg-gray-500 dark:bg-pink-500 dark:opacity-50"></hr>} 
+      {title !== "AO Solutions Oy" && <hr className="h-px border-0 my-10 mx-2 bg-gray-500 dark:bg-pink-500 dark:opacity-75"></hr>} 
       {/* TODO: Use last child selector instead */}
       <div className="absolute left-1/2 md:h-4/5 w-px dark:bg-pink-500 bg-gray-500 dark:opacity-75 transform -translate-x-1/2 dark:shadow-pink-500/30"></div>
     </div>
